d95d066b259dd30b88bfa450726dccb825f7e465
Tylor Schafer <tylorschafer@gmail.com>
2020-01-04 12:46:05 -0700

    Add github and production links to project model

--- projects/admin.py
+++ projects/admin.py
@@ -3,6 +3,6 @@ from projects.models import Project
 from django.db import models
 
 class ProjectAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'technology')
+    fields = ('title', 'description', 'technology', 'github', 'production')
 
 admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -7,6 +7,8 @@ class Project(models.Model):
     description = models.TextField()
     technology = models.CharField(max_length=20)
     image = models.FilePathField(path="/img")
+    github = models.URLField(max_length=200, blank=True, default='')
+    production = models.URLField(max_length=200, blank=True, default='')
 
     def __str__(self):
         return self.title
