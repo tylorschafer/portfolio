7f23b73ee11502777a26844847edde8f8a82c7db
Tylor Schafer <tylorschafer@gmail.com>
2020-01-04 00:28:50 -0700

    Remove staticfile_dirs

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -130,10 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
