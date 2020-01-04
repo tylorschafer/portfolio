5d83d8ef82c3647f385ba27af0dd371fe487f533
Tylor Schafer <tylorschafer@gmail.com>
2020-01-03 23:03:50 -0700

    Add blog models and views

---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from blog.models import Post
+
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "posts": posts
+    }
+    return render(request, "blog_category.html", context)
