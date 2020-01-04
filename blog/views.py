c553fc318cbaac02b252d801db83420d0324700e
Tylor Schafer <tylorschafer@gmail.com>
2020-01-03 23:05:38 -0700

    Update blog_detail view function to utilize form

--- blog/views.py
+++ blog/views.py
@@ -22,10 +22,22 @@ def blog_category(request, category):
 
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
         "comments": comments,
+        "form": form,
     }
-
     return render(request, "blog_detail.html", context)
