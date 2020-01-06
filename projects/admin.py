ebc9a948fb70a61b3bca8101d82598ccdad0d341
Tylor Schafer <tylorschafer@gmail.com>
2020-01-05 21:20:15 -0700

    Add better styling to project detail

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from projects.models import Project
+from projects.models import Technology
 from django.db import models
 
 class ProjectAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'technology', 'github', 'production')
+    fields = ('title', 'description', 'technologies', 'github', 'production')
+
+class TechnologyAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Technology, TechnologyAdmin)
--- projects/models.py
+++ projects/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 from django.db import models
 
+class Technology(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    technology = models.CharField(max_length=20)
+    technology = models.CharField(max_length=20, blank=True, default='')
+    technologies = models.ManyToManyField('Technology', related_name='projects', blank=True, default='')
     image = models.FilePathField(path="/img")
     github = models.URLField(max_length=200, blank=True, default='')
     production = models.URLField(max_length=200, blank=True, default='')
