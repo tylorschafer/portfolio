3efd7ed18fde7593ee8bb106e74a2cbf40a02034
Tylor Schafer <tylorschafer@gmail.com>
2020-01-23 09:56:55 -0700

    Add about_me object and context to view

--- about_me/views.py
+++ about_me/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from about_me.models import AboutMe
 
 def about_me_index(request):
-    return render(request, 'about_me.html')
+    about_me = AboutMe.objects.all()
+    context = {
+        'about_me': about_me,
+    }
+    return render(request, 'about_me.html', context)
